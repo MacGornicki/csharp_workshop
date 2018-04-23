916624d865bc8c8f71d86e6befa43132d22ba6f1
MacGornicki <maciej_gornicki@o2.pl>
2018-04-23 17:14:35 +0200

    Day1_2

--- CSharp_Fundamentals/ArrayListCollection.cs
+++ CSharp_Fundamentals/ArrayListCollection.cs
@@ -7,5 +7,70 @@ namespace CSharp_Fundamentals
     [TestFixture]
     public class ArrayListCollection
     {
+        [Test]
+        public void LoopBasics()
+        {
+            var theString =
+                "The interesting thing about London is that there are always stylish surprises around every corner.";
+            string[] result = theString.Split(' ');
+            int iterator = 0;
+            do
+            {
+
+                iterator++;
+
+            }
+            while (result[iterator] != "that");
+        }
+
+        [Test]
+        public void ForLoop()
+        {
+            var theString =
+                "The interesting thing about London is that there are always stylish surprises around every corner.";
+            string[] result = theString.Split(' ');
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(i);
+
+
+            }
+        }
+        [Test]
+        public void IfStatement()
+        {
+            var theString =
+                "The ththththth th interesting thing about London is that there are always stylish surprises around every corner.";
+            string[] result = theString.Split(' ');
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] == "London")
+                {
+                    Console.WriteLine("nice city");
+
+                }
+                else if (result[i].Contains("th"))
+                {
+
+                    if (result[i].Length >= 5)
+                    {
+                        Console.WriteLine("more than 4 chars");
+                        Console.WriteLine("bad city");
+                    }
+                    else
+                    {
+                        Console.WriteLine(result[i]);
+                    }
+                        
+                    
+                }
+                else
+                {
+                    Console.WriteLine("nothing");
+                }
+                Console.WriteLine(i);
+
+            }
+        }
     }
 }
